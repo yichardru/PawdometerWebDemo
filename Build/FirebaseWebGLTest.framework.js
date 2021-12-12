13049a29cc5b18f88442a91264586e2bd3bea417
thicczucc <56744122+thicczucc@users.noreply.github.com>
2021-12-11 20:10:20 -0800

    updated leaderboard script

--- Build/FirebaseWebGLTest.framework.js
+++ Build/FirebaseWebGLTest.framework.js
@@ -3498,12 +3498,14 @@ function _GetJSON(path, objectName, callback, fallback) {
  var parsedObjectName = Pointer_stringify(objectName);
  var parsedCallback = Pointer_stringify(callback);
  var parsedFallback = Pointer_stringify(fallback);
+ window.alert("Getting JSON info");
  try {
   firebase.database().ref(parsedPath).once("value").then((function(snapshot) {
-   console.log(JSON.stringify(snapshot.val()));
+   window.alert(JSON.stringify(snapshot.val()));
    unityInstance.Module.SendMessage(parsedObjectName, parsedCallback, JSON.stringify(snapshot.val()));
   }));
  } catch (error) {
+  window.alert(JSON.stringify(error, Object.getOwnPropertyNames(error)));
   unityInstance.Module.SendMessage(parsedObjectName, parsedFallback, JSON.stringify(error, Object.getOwnPropertyNames(error)));
  }
 }
